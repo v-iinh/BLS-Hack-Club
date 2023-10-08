1e786acf365b5f322454edc76a84209b2f71fd45
ilyKcaj <141356468+ilyKcaj@users.noreply.github.com>
2023-10-08 15:54:32 -0400

    about.html page update
    updated dropdown functionality (added animation on click + only have 1 dropdown open at a time)
    
    checked f&q to faq
    
    changed where the meets are

--- accordion.js
+++ accordion.js
@@ -1,15 +1,20 @@
 var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+            for (let j = 0; j < acc.length; j++) {
+            acc[j].classList.remove("active");
+            if(j!=i){
+                acc[j].nextElementSibling.style.maxHeight = null;
+            }
+        }
 
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            this.classList.toggle("active");
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
