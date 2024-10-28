246c3e0f2f0ebd6348d8b2c40e6cd9c10bc9a105
jacky <jackymon260@gmail.com>
2024-10-27 22:53:26 -0400

    mobile nav bar update
    - added projects and emails option
    - made nav bar mobile open take up the whole screen
    - fixed changing resolution bug

--- visuals/mobileNav.js
+++ visuals/mobileNav.js
@@ -3,10 +3,13 @@ const menu = document.getElementsByClassName('mobile_expand')[0];
 const bars = document.getElementsByClassName('nav_mobile')[0];
 
 bars.addEventListener('click', function(){
+    console.log(toggleDisplay);
     if(toggleDisplay % 2 === 0){
-        menu.style.maxHeight = "180px";
+        menu.style.maxHeight = "100vh";
+        document.body.style.overflow = 'hidden'; // Disable scroll
     } else {
         menu.style.maxHeight = "0px";
+        document.body.style.overflow = ''; // Re-enable scroll 
     }
     toggleDisplay++;
 });
